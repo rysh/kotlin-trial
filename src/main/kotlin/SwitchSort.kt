e5959e907599b537df393caf103fe639d2bf52a5
rysh <rysh.cact@gmail.com>
2020-02-20 03:54:21 +0900

    fixed switch sort

--- src/main/kotlin/SwitchSort.kt
+++ src/main/kotlin/SwitchSort.kt
@@ -1,111 +1,28 @@
 fun SwitchSort(arr: Array<Int>): Int {
+    val sorted: IntArray = arr.toIntArray()
+    sorted.sort()
+    val sorting = arr.toIntArray()
+    var count = 0
+    outer@ while (!isSorted(sorting, sorted)) {
+        sorting.forEachIndexed {idx, num ->
+            if (sorted[idx] != sorting[idx]) {
+                val idx2 = sorted.lastIndexOf(sorting[idx])
+                swap(sorting, idx, idx2)
+                count+=1
+                return@forEachIndexed
+            }
+        }
 
-    val list = SwitchSortedList(arr.toList())
-    list.sort()
-
-    return list.count
+    }
+    return count
 }
 
+fun isSorted(arr1: IntArray, arr2: IntArray) = arr1.contentEquals(arr2)
 
-class SwitchSortedList(val origin: List<Int>) {
-    var count = 0
-    class Node(val value:Int) {
-        var next:Node? = null
-
-        fun add(num: Int) {
-            if (next == null) {
-                next = Node(num)
-            } else {
-                next!!.add(num)
-            }
-        }
-        fun min():Int {
-            if (next == null) {
-                return value;
-            } else{
-                val minNum = next!!.min()
-                if (value <= minNum) {
-                    return value
-                } else {
-                    return minNum
-                }
-            }
-        }
-        fun isSmall():Boolean {
-            if (next == null) {
-                return true
-            }
-            return min() >= value
-        }
-
-        fun print() {
-            print("$value ")
-            if (next != null) {
-                next!!.print()
-            } else {
-                println("")
-            }
-        }
-
-    }
-    var node = Node(origin.first())
-    init {
-        for (n in origin.drop(1)) {
-            node.add(n)
-        }
-    }
-
-    fun sort() {
-        var curr: Node? = node.next
-        while (curr != null) {
-            while (!isSmall(curr!!)) {
-                stepForeword(curr)
-                count+=1
-            }
-            curr = curr.next
-//            else {
-//                stepForeword(curr)
-//                count += 1
-//            }
-        }
-    }
-
-    private fun isSmall(target: Node): Boolean {
-        var curr: Node? = node
-        while (curr != null && curr != target) {
-            if (curr.value > target.value) {
-                return false
-            }
-            curr = curr.next
-        }
-        return true
-    }
-
-
-    fun stepForeword(target: SwitchSortedList.Node) {
-        if (node.next!!.value == target.value) {
-            val temp = target.next
-            node.next = temp
-            target.next = node
-            node = target
-        } else {
-            var prev = node
-            var curr: Node? = node.next
-            while (curr!!.next != null) {
-                if (curr.next!!.value == target.value) {
-                    val temp = target.next
-                    target.next = curr
-                    curr.next = temp
-                    prev.next = target
-                    break
-                }
-                prev = curr
-                curr = curr.next
-            }
-        }
-    }
-
-    fun print() {
-        node.print()
-    }
+fun swap(arr1: IntArray, idx1:Int, idx2:Int): IntArray {
+    val value1 = arr1[idx1]
+    val value2 = arr1[idx2]
+    arr1[idx1] = value2
+    arr1[idx2] = value1
+    return arr1
 }
--- src/test/kotlin/SwitchSortKtTest.kt
+++ src/test/kotlin/SwitchSortKtTest.kt
@@ -5,8 +5,28 @@ import org.junit.Assert.*
 class SwitchSortKtTest {
 
     @Test
+    fun isSortedTest() {
+        assertEquals(true, isSorted(intArrayOf(1,2,3), intArrayOf(1,2,3)))
+        assertEquals(false, isSorted(intArrayOf(2,1,3), intArrayOf(1,2,3)))
+    }
+    @Test
+    fun swapTest() {
+        assertArrayEquals(intArrayOf(1,2,3), swap(intArrayOf(3,2,1), 0,2))
+    }
+    @Test
     fun switchSortTest() {
         assertEquals(2, SwitchSort(arrayOf(3,1,2)))
         assertEquals(2, SwitchSort(arrayOf(1,3,4,2)))
+
+        assertEquals(1, SwitchSort(arrayOf(1,2,4,3)))
+        assertEquals(0, SwitchSort(arrayOf(1,2,3,4)))
+        assertEquals(3, SwitchSort(arrayOf(3,4,2,1)))
+        assertEquals(2, SwitchSort(arrayOf(1,3,4,2)))
+        assertEquals(2, SwitchSort(arrayOf(3,1,2)))
+        assertEquals(1, SwitchSort(arrayOf(2,1)))
+        assertEquals(2, SwitchSort(arrayOf(4,3,2,1)))
+        assertEquals(2, SwitchSort(arrayOf(5,4,3,2,1)))
+        assertEquals(4, SwitchSort(arrayOf(5,3,4,1,2)))
+        assertEquals(3, SwitchSort(arrayOf(5,4,3,1,2)))
     }
 }
